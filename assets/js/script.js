4e2bcfa034f55609f27aeb3bb941bbab2599e620
vikernes <armic27666@gmail.com>
2024-05-05 16:15:47 -0500

    dark mode and menu added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-
+// Función modo claro y oscuro
+function toggleDarkMode() {
+    var body = document.body;
+    body.classList.toggle("dark-mode");
+    var darkModeLink = document.getElementById("dark-mode-link");
+    if (body.classList.contains("dark-mode")) {
+        darkModeLink.textContent = "Modo claro | ";
+    } else {
+        darkModeLink.textContent = "Modo oscuro | ";
+    }
+}
+// Asignar la función toggleDarkMode al clic del enlace "Modo oscuro"
+document.getElementById("dark-mode-link").addEventListener("click", toggleDarkMode);
